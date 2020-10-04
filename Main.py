1795df71f55c6e040422749d8210b74178c5eb58
Douglas de León <dele18037@uvg.edu.gt>
2020-10-04 12:55:11 -0600

    escena

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import random
 
 brick = Material(diffuse = color(0.8, 0.25, 0.25 ), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 32)
+floor = Material(diffuse = color(0.75, 0.75, 0.75 ), spec = 64)
 mirror = Material(spec = 64, matType = REFLECTIVE)
 
 glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT) 
@@ -17,20 +18,19 @@ r = Raytracer(width,height)
 r.glClearColor(0.2, 0.6, 0.8)
 r.glClear()
 
-r.envmap = Envmap('envmap.bmp')
-
-r.pointLight = PointLight(position = V3(0,0,0), intensity = 1)
+# r.envmap = Envmap('envmap.bmp')
+r.pointLight = PointLight(position = V3(0,-1,2), intensity = 1)
 r.ambientLight = AmbientLight(strength = 0.1)
 
-#r.scene.append( Sphere(V3( 1, 1, -10), 1.5, brick) )
-#r.scene.append( Sphere(V3( 0, -1, -5),  1, glass) )
-#r.scene.append( Sphere(V3(-3, 3, -10),  2, mirror) )
 
-#r.scene.append( Plane( V3(-2,-3,0), V3(1,1,0), stone))
+r.scene.append(Plane( V3(2,0,-2), V3(-1,0,0), floor)) # derecha
+r.scene.append(Plane( V3(-2,0,-2), V3(1,0,0), floor)) # izquierda
+r.scene.append(Plane( V3(-1,1,-10), V3(0,0,1), floor)) # Centro
+r.scene.append(Plane( V3(0,-2,-2), V3(0,1,0), floor)) # Piso
+r.scene.append(Plane( V3(0,2,-2), V3(0,-1,0), floor)) # Techo
 
-r.scene.append( AABB(V3(0, 1.5, -5), 1.5, stone ) )
-r.scene.append( AABB(V3(1.5, -1.5, -5), 1.5, mirror ) )
-r.scene.append( AABB(V3(-1.5, -1.5, -5), 1.5, glass ) )
+r.scene.append(AABB(V3(0.5, -1, -4), 0.5, brick))
+r.scene.append(AABB(V3(-0.5, -1, -4), 0.5, stone))
 
 
 r.rtRender()
