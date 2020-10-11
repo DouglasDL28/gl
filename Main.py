2e9b665d7c2214c0e64ae36e84a605796ed1d326
Douglas de León <dele18037@uvg.edu.gt>
2020-10-10 19:05:28 -0600

    Moby Dick

--- Main.py
+++ Main.py
@@ -8,29 +8,50 @@ brick = Material(diffuse = color(0.8, 0.25, 0.25 ), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 32)
 floor = Material(diffuse = color(0.75, 0.75, 0.75 ), spec = 64)
 mirror = Material(spec = 64, matType = REFLECTIVE)
-
 glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT) 
 
+whale_belly = Material(diffuse= color(0.887, 0.973, 1), spec = 128)
+whale_skin = Material(diffuse=color(1, 1, 1), spec = 32)
+whale_eye = Material(diffuse=color(0.3, 0.3, 0.3), spec=256)
 
-width = 256
-height = 256
+boxMat = Material(texture = Texture('box.bmp'))
+
+earthMat = Material(texture = Texture('earthDay.bmp'))
+
+
+width = 128
+height = 128
 r = Raytracer(width,height)
-r.glClearColor(0.2, 0.6, 0.8)
+# r.glClearColor(0.2, 0.6, 0.8)
 r.glClear()
 
-# r.envmap = Envmap('envmap.bmp')
-r.pointLight = PointLight(position = V3(0,-1,2), intensity = 1)
-r.ambientLight = AmbientLight(strength = 0.1)
+# r.envmap = Envmap('starSky.bmp')
+
+# Lights
+#r.pointLights.append( PointLight(position = V3(-4,4,0), intensity = 0.5))
+#r.pointLights.append( PointLight(position = V3( 4,0,0), intensity = 0.5))
+r.dirLight = DirectionalLight(direction = V3(1, -1, -2), intensity = 0.5)
+r.ambientLight = AmbientLight(strength = 0.2)
+
+# Objects
+#r.scene.append( Sphere(V3( 0, 0, -8), 2, brick) )
+#r.scene.append( Sphere(V3( -0.5, 0.5, -5), 0.25, stone))
+#r.scene.append( Sphere(V3( 0.25, 0.5, -5), 0.25, stone))
+
+#Moby Dick
+r.scene.append( AABB(V3( 0,-3,-14), V3(4, 8, 1), whale_belly)) # belly
+r.scene.append( AABB(V3( 0,-3,-16), V3(6, 12, 1), whale_skin)) # body
+r.scene.append( AABB(V3(-3,-3,-15), V3(1, 2, 1), whale_skin)) # left flipper
+r.scene.append( AABB(V3( 3,-3,-15), V3(1, 2, 1), whale_skin)) # right flipper
+r.scene.append( AABB(V3(-3, 0,-16), V3(1, 1, 1), whale_eye)) #left eye
+r.scene.append( AABB(V3( 3, 0,-16), V3(1, 1, 1), whale_eye)) # right eye
 
 
-r.scene.append(Plane( V3(2,0,-2), V3(-1,0,0), floor)) # derecha
-r.scene.append(Plane( V3(-2,0,-2), V3(1,0,0), floor)) # izquierda
-r.scene.append(Plane( V3(-1,1,-10), V3(0,0,1), floor)) # Centro
-r.scene.append(Plane( V3(0,-2,-2), V3(0,1,0), floor)) # Piso
-r.scene.append(Plane( V3(0,2,-2), V3(0,-1,0), floor)) # Techo
+#Perquod
+# r.scene.append( AABB(V3(0, -3, -10), V3(5, 0.1, 5) , boxMat ) )
 
-r.scene.append(AABB(V3(0.5, -1, -4), 0.5, brick))
-r.scene.append(AABB(V3(-0.5, -1, -4), 0.5, stone))
+# r.scene.append( Sphere(V3( 0, 0, -8), 2, earthMat))
+
 
 
 r.rtRender()
