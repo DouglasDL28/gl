a11b621426acbd3b1700eddf43f1eebd7c0b7bdb
Douglas de León <dele18037@uvg.edu.gt>
2020-10-26 18:21:21 -0600

    Menú principal iniciado

--- RayCaster.py
+++ RayCaster.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 
 from math import cos, sin, pi
+
+from pygame.constants import KEYDOWN, K_ESCAPE, MOUSEBUTTONDOWN, MOUSEBUTTONUP, QUIT
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -131,56 +134,106 @@ def updateFPS():
 r = Raycaster(screen)
 r.load_map('maps/map2.txt')
 
-isRunning = True
-while isRunning:
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            isRunning = False
+bg = pygame.image.load('textures/diamond.png')
 
-        newX = r.player['x']
-        newY = r.player['y']
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
 
-        if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
+
+def main_menu ():
+    click = False
+    while True:
+        screen.blit(bg, (0, 0))
+        draw_text('Menú principal', font, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+
+        if button_1.collidepoint(mx, my): # Inicio
+            if click: 
+                game()
+        if button_2.collidepoint(mx, my): # Salida
+            if click:
+                pygame.quit()
+                sys.exit()
+
+        pygame.draw.rect(screen, (255,0,0), button_1)
+        pygame.draw.rect(screen, (255,0,0), button_2)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+def game():
+    isRunning = True
+    while isRunning:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
                 isRunning = False
-            elif ev.key == pygame.K_w:
-                newX += cos(r.player['angle'] * pi / 180) * r.stepSize
-                newY += sin(r.player['angle'] * pi / 180) * r.stepSize
-            elif ev.key == pygame.K_s:
-                newX -= cos(r.player['angle'] * pi / 180) * r.stepSize
-                newY -= sin(r.player['angle'] * pi / 180) * r.stepSize
-            elif ev.key == pygame.K_a:
-                newX -= cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
-                newY -= sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
-            elif ev.key == pygame.K_d:
-                newX += cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
-                newY += sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
-            elif ev.key == pygame.K_q:
-                r.player['angle'] -= 5
-            elif ev.key == pygame.K_e:
-                r.player['angle'] += 5
+
+            newX = r.player['x']
+            newY = r.player['y']
+
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_ESCAPE:
+                    isRunning = False
+                elif ev.key == pygame.K_w:
+                    newX += cos(r.player['angle'] * pi / 180) * r.stepSize
+                    newY += sin(r.player['angle'] * pi / 180) * r.stepSize
+                elif ev.key == pygame.K_s:
+                    newX -= cos(r.player['angle'] * pi / 180) * r.stepSize
+                    newY -= sin(r.player['angle'] * pi / 180) * r.stepSize
+                elif ev.key == pygame.K_a:
+                    newX -= cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
+                    newY -= sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
+                elif ev.key == pygame.K_d:
+                    newX += cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
+                    newY += sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
+                elif ev.key == pygame.K_q:
+                    r.player['angle'] -= 5
+                elif ev.key == pygame.K_e:
+                    r.player['angle'] += 5
 
 
-            i = int(newX / r.blocksize)
-            j = int(newY / r.blocksize)
+                i = int(newX / r.blocksize)
+                j = int(newY / r.blocksize)
 
-            if r.map[j][i] == ' ':
-                r.player['x'] = newX
-                r.player['y'] = newY
+                if r.map[j][i] == ' ':
+                    r.player['x'] = newX
+                    r.player['y'] = newY
 
-    screen.fill(pygame.Color("gray")) # Background
+        screen.fill(pygame.Color("gray")) # Background
 
-    screen.fill(pygame.Color("skyblue"), (int(r.width / 2), 0, int(r.width / 2),int(r.height / 2))) # Sky
-    
-    screen.fill(pygame.Color("green"), (int(r.width / 2), int(r.height / 2), int(r.width / 2),int(r.height / 2))) # Floor
+        screen.fill(pygame.Color("skyblue"), (int(r.width / 2), 0, int(r.width / 2),int(r.height / 2))) # Sky
+        
+        screen.fill(pygame.Color("green"), (int(r.width / 2), int(r.height / 2), int(r.width / 2),int(r.height / 2))) # Floor
 
-    r.render()
-    
-    # FPS
-    screen.fill(pygame.Color("black"), (0,0,30,30))
-    screen.blit(updateFPS(), (0,0))
-    clock.tick(30)
-    
-    pygame.display.update()
+        r.render()
+        
+        # FPS
+        screen.fill(pygame.Color("black"), (0,0,30,30))
+        screen.blit(updateFPS(), (0,0))
+        clock.tick(30)
+        
+        pygame.display.update()
 
-pygame.quit()
+main_menu()
+
+
