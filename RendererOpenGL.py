6e9c80804f6bff2d3bedc3956fdeb8a224290013
Douglas De Leon <drdeleon@plusti.com>
2020-11-26 19:37:10 -0600

    Mouse

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -25,16 +25,17 @@ r.modelList.append(Model('./models/Face/face.obj',
                          scale=glm.vec3(2,2,2)))
 r.modelList.append(Model('./models/Gold/gold.obj',
                          './models/Gold/gold_tex.bmp',
-                         scale=glm.vec3(1,1,1)))
+                         rotation=glm.vec3(-45,0,90),
+                         scale=glm.vec3(0.2,0.2,0.2)))
 r.modelList.append(Model('./models/Rock/rock.obj',
                          './models/Rock/rock_tex.bmp',
-                         scale=glm.vec3(0.05,0.05,0.05)))
+                         scale=glm.vec3(0.01,0.01,0.01)))
 r.modelList.append(Model('./models/Stone/stone.obj',
                          './models/Stone/stone_tex.bmp',
-                         scale=glm.vec3(1,1,1)))
+                         scale=glm.vec3(0.4,0.4,0.4)))
 
 
-
+isDragging = False
 isPlaying = True
 while isPlaying:
 
@@ -67,11 +68,23 @@ while isPlaying:
                 r.wireframeMode()
             elif ev.key == pygame.K_LEFT:
                 r.modelIdx = (r.modelIdx - 1) % len(r.modelList)
-                print(r.modelIdx)
             elif ev.key == pygame.K_RIGHT:
                 r.modelIdx = (r.modelIdx + 1) % len(r.modelList)
             elif ev.key == pygame.K_ESCAPE:
                 isPlaying = False
+
+        elif ev.type == pygame.MOUSEBUTTONDOWN:
+            isDragging = True
+            x, y = pygame.mouse.get_rel()
+        elif ev.type == pygame.MOUSEBUTTONUP:
+            isDragging = False
+        
+        elif ev.type == pygame.MOUSEMOTION:
+            if isDragging:
+                x, y = pygame.mouse.get_rel()
+                r.camPosition.x -= x/r.width * 10
+                r.camPosition.y += y/r.height * 10
+
 
     # Main Renderer Loop
     r.render()
